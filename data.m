b0fa70c1ac186b274721175b6c4d0d7375464236
Baian Chen <baian@mit.edu>
2016-10-29 23:28:18 -0400

    done

--- data.m
+++ data.m
@@ -1,13 +1,14 @@
 load('X.dat')
 load('Y.dat')
-load('Z.dat')
-load('I.dat')
-img = zeros(256,373,3);
-img(:,:,1) = I(1:256,:);
-img(:,:,2) = I(257:512,:);
-img(:,:,3) = I(513:768,:);
-
-figure
-surf(X(2:end-1, 2:end-1),Z(2:end-1, 2:end-1),Y(2:end-1, 2:end-1)),double(img(2:end-1, 2:end-1, :))/256)
-axis('equal')
-shading interp
+load('Z.dat')  
+A=X( : ,1);
+B=Y( : ,1);
+C=Z( : ,1);
+D=[ 1,0 ];
+E=[ 1,1 ];
+X = kron(X,E);
+Y = kron(Y,E);
+Z = kron(Z,D);
+X=[X A];
+Y=[Y B];
+Z=[Z C];
